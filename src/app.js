7dfba6851c0f28dbb506b85006befb6b708689b3
Sarl23 <sergioandresrojasleon@gmail.com>
2024-09-08 21:48:00 -0500

    chore: update new routes to model api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const userRoutes = require('../src/routes/index.js')
+const routes = require('../src/routes/index.js');
 const {swaggerUi, specs } = require('./swagger');
 const path = require('path');
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use('/api', routes);
 
 app.use('/docs', express.static(path.join(__dirname, 'node_modules/swagger-ui-dist')));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
