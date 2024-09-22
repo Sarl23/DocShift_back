87ec385ea01fe581b4c1b2c8f76f648d928585c9
Sarl23 <sergioandresrojasleon@gmail.com>
2024-09-21 20:49:46 -0500

    Implement new controller router for usertype

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
 const companyRoutes = require('./companyRoutes');
+const userTypeRoutes = require('./userTypeRoutes');
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(companyRoutes);
+router.use(userTypeRoutes);
 
 module.exports = router;
