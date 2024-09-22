5a96a7d16e4df08e94a481946a17adf2994fde07
Sarl23 <sergioandresrojasleon@gmail.com>
2024-09-22 13:37:55 -0500

    Import routes to shiftTypes in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 const companyRoutes = require('./companyRoutes');
 const userTypeRoutes = require('./userTypeRoutes');
+const shiftTypeRoutes = require('./shiftTypeRoutes');
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(companyRoutes);
 router.use(userTypeRoutes);
+router.use(shiftTypeRoutes);
 
 module.exports = router;
