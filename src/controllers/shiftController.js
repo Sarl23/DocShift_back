a8b69454c92532800a8fd78d80e031e46c257db3
Sarl23 <sergioandresrojasleon@gmail.com>
2024-10-09 23:40:31 -0500

    chore: order code and fix endpoint shift create

--- src/controllers/shiftController.js
+++ src/controllers/shiftController.js
@@ -1,14 +1,15 @@
-const {getDb} = require('../firebase.js');
+const { getDb } = require('../firebase.js');
+const { v4: uuidv4 } = require('uuid');
 
 const getAllShift = async (req, res) => {
-    try{
-        const {companyId} = req.params;
-        const db =  await getDb();
-        if(!db){
+    try {
+        const { companyId } = req.params;
+        const db = await getDb();
+        if (!db) {
             throw new Error('FireStore has no been initialized');
         }
         const querySnapshot = await db.collection('companies').doc(companyId).collection('shift').get();
-        const shiftData = await Promise.all(querySnapshot.docs.map(async (doc) =>{
+        const shiftData = await Promise.all(querySnapshot.docs.map(async (doc) => {
             const shift = doc.data();
             return {
                 id: doc.id,
@@ -16,37 +17,46 @@ const getAllShift = async (req, res) => {
             }
         }));
         res.json(shiftData);
-    }catch(error){
+    } catch (error) {
         console.error('Error getting shift', error);
         res.status(500).send('Internal Server Error getting shift');
     }
 };
 
 const postCreateShift = async (req, res) => {
-    try{
-        const {companyId} = req.params;
+    try {
+        const { companyId } = req.params;
         const db = await getDb();
-        if(!db){
+        if (!db) {
             throw new error('FireStore has no been initialized initialized');
         }
-        const newShift = req.body;
-        const newShiftRef = await db.collection('companies').doc(companyId).collection('shift').add(newShift);
-        res.status(201).json({id: newShiftRef.id, ...newShift})
-    }catch(error){
+        const { user_id, ...newShift } = req.body;
+        if(!user_id){
+            res.status(400).json({error: 'El id del usuario es requerido'});
+        }
+        const autoGenerateId = uuidv4();
+        const formattedDate = new Date().toLocaleDateString();
+
+        const newFormatReference = `${autoGenerateId}_${user_id}_${formattedDate}}`;
+        const newShiftRef = await db.collection('companies').doc(companyId).collection('shift').doc(newFormatReference);
+        await newShiftRef.set({ id: newShift.id, ...newShift });
+
+        res.status(201).json({ id: newShiftRef.id, ...newShift })
+    } catch (error) {
         console.error('Error creating shift', error);
         res.status(500).send('Internal server error creating shift')
     }
 };
 
-const getShiftById = async (req, res) =>{
-    try{
-        const {companyId, shiftId} = req.params;
+const getShiftById = async (req, res) => {
+    try {
+        const { companyId, shiftId } = req.params;
         const db = await getDb();
-        if(!db){
+        if (!db) {
             throw new Error('FireStore has no been initialized');
         }
-        const shiftDoc =  await db.collection('companies').doc(companyId).collection('shift').doc(shiftId).get();
-        if(!shiftDoc.exists){
+        const shiftDoc = await db.collection('companies').doc(companyId).collection('shift').doc(shiftId).get();
+        if (!shiftDoc.exists) {
             return res.status(400).send({
                 "success": true,
                 "error": {
@@ -55,45 +65,45 @@ const getShiftById = async (req, res) =>{
                 }
             });
         }
-        res.json({id: shiftDoc.id, ...shiftDoc.data()})
-    }catch(error){
+        res.json({ id: shiftDoc.id, ...shiftDoc.data() })
+    } catch (error) {
         console.log('Error fetching shiftTye');
         res.status(500).send('Internal server error');
     }
 };
 
-const updateShiftById = async (req, res) =>{
-    try{
-        const {companyId, shiftId} = req.params;
+const updateShiftById = async (req, res) => {
+    try {
+        const { companyId, shiftId } = req.params;
         const db = await getDb();
-        if(!db){
+        if (!db) {
             throw new Error('FireStore has no been initialized');
         }
         const shiftData = req.body;
         const shiftRef = await db.collection('companies').doc(companyId).collection('shift').doc(shiftId).update(shiftData);
-        res.status(201).json({id: shiftRef.id, ...shiftData});
-    }catch(error){
+        res.status(201).json({ id: shiftRef.id, ...shiftData });
+    } catch (error) {
         console.error('Error updating shift');
         res.status(500).send('Internal server error');
     }
 };
 
-const deleteShift =async (req, res)=>{
-    try{
-        const {companyId, shiftId}= req.params;
+const deleteShift = async (req, res) => {
+    try {
+        const { companyId, shiftId } = req.params;
         const db = await getDb();
-        if(!db){
+        if (!db) {
             throw new Error('FireStore has no been initialized');
         }
         await db.collection('companies').doc(companyId).collection('shift').doc(shiftId).delete();
         res.status(204).send('Successfully deleted');
-    }catch{
+    } catch {
         console.error('Error deleting shift');
         res.status(500).send('Internal server error deleting shift');
     }
 }
 
-module.exports={
+module.exports = {
     getAllShift,
     postCreateShift,
     getShiftById,
